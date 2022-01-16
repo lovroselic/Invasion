a08c2ca21f5e17438e1672a6ab31fb2ba0843b0c
lovroselic <lovro.selic@gmail.com>
2022-01-16 15:39:55 +0100

    0.00.00 cold start from template

---
+++ assets_Invasion.js
@@ -0,0 +1,23 @@
+//Assets for GhostRun2
+console.log("Assets for template ready.");
+
+var LoadTextures = [
+];
+var LoadSprites = [
+
+];
+var LoadSequences = [];
+var LoadSheets = [
+
+];
+var LoadRotated = [];
+var LoadPacks = [];
+var LoadExtWasm = [];
+var LoadAudio = [
+];
+var ExtendSheetTag = [];
+var LoadSheetSequences = [
+
+];
+var LoadFonts = [
+];
